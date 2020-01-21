c4907600defa46069b2a81971b00089c014d2c73
srinivas.k12 <srinivasarao.k12@infosys.com>
2020-01-21 21:12:26 +0530

    proxy server implemented

--- apps/stocks-api/src/environments/environment.ts
+++ apps/stocks-api/src/environments/environment.ts
@@ -3,5 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'Tpk_6f065baf291f48a7b768fd32d071a316',
+  apiURL: 'https://sandbox.iexapis.com'
 };
